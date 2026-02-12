8f7f41ad74ca160177f4529c455a3df9f0988452
rosaomely <OMELY.PALACIOS@GMAIL.COM>
2026-02-11 21:35:49 -0400

    proyecto

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { AppComponent } from './app/app'; // Fíjate que termina en /app que es tu archivo app.ts
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
